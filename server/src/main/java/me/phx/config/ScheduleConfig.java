71150cdee89d163200e86bd78f5171be15392c78
Alwayswithme <phx13ye@gmail.com>
2015-08-11 22:59:28 +0800

    use conditional

--- server/src/main/java/me/phx/config/ScheduleConfig.java
+++ server/src/main/java/me/phx/config/ScheduleConfig.java
@@ -1,11 +1,11 @@
 package me.phx.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@Profile("production")
 @Configuration
+@ConditionalOnProperty("enable.schedule")
 public class ScheduleConfig {
 }
