6eddad2e9b72040b3fe1b317602fe296c6a3be1a
Alwayswithme <phx13ye@gmail.com>
2015-08-30 22:47:15 +0800

    spring redis

--- server/src/main/java/me/phx/controler/RedisController.java
+++ server/src/main/java/me/phx/controler/RedisController.java
@@ -1,6 +1,7 @@
 package me.phx.controler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -53,5 +55,52 @@ public class RedisController {
 
         System.out.println(redisAtomicInteger.getAndSet(0));
         System.out.println(redisAtomicInteger.get());
+    }
+
+    @RequestMapping("run3")
+    public void run3() {
+        String key = "connections";
+
+        System.out.println(valueOperations.setIfAbsent(key, String.valueOf(10)));
+        System.out.println(valueOperations.increment(key, 1));
+        System.out.println(valueOperations.increment(key, 1));
+
+        stringRedisTemplate.delete(key);
+
+        System.out.println(valueOperations.increment(key, 1));
+    }
+
+    @RequestMapping("run4")
+    public void run4() throws InterruptedException {
+        String key = "resource:lock";
+
+        System.out.println(valueOperations.setIfAbsent(key, "Redis Demo"));
+        System.out.println(stringRedisTemplate.expire(key, 10, TimeUnit.SECONDS));
+        System.out.println(stringRedisTemplate.getExpire(key));
+
+        valueOperations.set(key, "Redis Demo 1");
+        System.out.println(stringRedisTemplate.expire(key, 10, TimeUnit.SECONDS));
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println(stringRedisTemplate.getExpire(key));
+
+        valueOperations.set(key, "Redis Demo 2");
+        System.out.println(stringRedisTemplate.getExpire(key));
+    }
+
+    @RequestMapping("run5")
+    public void run5() throws InterruptedException {
+        String key = "friends";
+        ListOperations<String, String> opsForList = stringRedisTemplate.opsForList();
+
+        System.out.println(opsForList.rightPush(key, "Alice"));
+        System.out.println(opsForList.rightPush(key, "Bob"));
+        System.out.println(opsForList.leftPush(key, "Sam"));
+
+        System.out.println(opsForList.size(key));
+
+        System.out.println(opsForList.range(key, 0, -1));
+        System.out.println(opsForList.range(key, 0, 1));
+        System.out.println(opsForList.range(key, 1, 2));
     }
 }
